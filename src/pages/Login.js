af10909076853aa192536c0bd4ae79d80c85cd90
Juliano Boese <juliano.boese@gmail.com>
2022-03-04 16:01:02 -0300

    Requisito 1 - Cria a página de Login e suas funcionalidades

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { userLoginAction } from '../actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/igm;
+    return emailRegex.test(email);
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email } = this.state;
+    const { userLogin, history } = this.props;
+    userLogin(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const minPasswordLength = 6;
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <input
+          type="email"
+          name="email"
+          placeholder="e-mail"
+          value={ email }
+          onChange={ this.handleChange }
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="senha"
+          value={ password }
+          onChange={ this.handleChange }
+          data-testid="password-input"
+        />
+        <button
+          type="submit"
+          disabled={ !this.validateEmail(email) || password.length < minPasswordLength }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  userLogin: (email) => dispatch(userLoginAction(email)),
+});
+
+Login.propTypes = {
+  userLogin: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
